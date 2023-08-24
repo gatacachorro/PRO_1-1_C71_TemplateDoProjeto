2a5a0ab340545bd55541f4d3824255af5644c16a
gatacachorro <100207223+gatacachorro@users.noreply.github.com>
2023-08-24 14:01:13 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
-  
+const firebaseConfig = {
+  apiKey: "AIzaSyD1f7kumhfXYguPyqv-mVtlyr9N6gGocCw",
+  authDomain: "pro-c71-84bf2.firebaseapp.com",
+  projectId: "pro-c71-84bf2",
+  storageBucket: "pro-c71-84bf2.appspot.com",
+  messagingSenderId: "443840800493",
+  appId: "1:443840800493:web:cb6e6007815d388ffb0ee4"
 };
 
 firebase.initializeApp(firebaseConfig);
